07b8812b9f1905ad3a5cd3334a32e1e24cb1562d
oStglnd <stigland@kth.se>
2023-03-28 16:29:49 +0200

    finished basic assignment

--- a1/main.py
+++ a1/main.py
@@ -1,6 +1,7 @@
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 # get files
 from model import linearClassifier
@@ -30,3 +31,66 @@ linearModel = linearClassifier(
     K=Y_train.shape[1], 
     d=X_train.shape[1]
 )
+
+###
+W_grads, b_grads = linearModel.computeGrads(
+    X=X_train[:100], 
+    Y=Y_train[:100], 
+    lambd=0.1
+)
+
+# ###
+# W_gradsNum, b_gradsNum = linearModel.computeGradsNumerical(
+#     X=X_train[:100], 
+#     Y=Y_train[:100], 
+#     lambd=0.1
+# )
+
+# ### test gradient diff
+# W_gradDiffMax = np.max(np.abs(W_grads - W_gradsNum))
+# b_gradDiffMax = np.max(np.abs(b_grads - b_gradsNum))
+
+
+### train model
+lambd       = 1.0
+n_batch     = 100
+eta         = 0.001
+n_epochs    = 40
+
+trainLoss, valLoss, testAcc = [], [], []
+for epoch in range(n_epochs):
+    for i in range(10000 // n_batch):
+        X_trainBatch = X_train[i*n_batch:i*n_batch+n_batch]
+        Y_trainBatch = Y_train[i*n_batch:i*n_batch+n_batch]
+        
+        linearModel.train(
+            X=X_trainBatch, 
+            Y=Y_trainBatch, 
+            lambd=lambd, 
+            eta=eta
+        )
+        
+    trainLoss.append(linearModel.computeCost(X_train, Y_train, lambd=lambd))
+    valLoss.append(linearModel.computeCost(X_val, Y_val, lambd=lambd))
+    testAcc.append(linearModel.computeAcc(X_test, k_test))
+    
+    print(
+        'EPOCH {} - trainingloss: {:.2f}, validationLoss: {:.2f}, testAcc: {:.2f}'\
+        .format(epoch, trainLoss[-1], valLoss[-1], testAcc[-1]))
+        
+        
+plt.plot(trainLoss, 'b', linewidth=1.5, alpha=1.0, label='Training loss')
+plt.plot(valLoss, 'r', linewidth=1.5, alpha=1.0, label='Validation loss')
+
+plt.xlim(0, n_epochs)
+plt.xlabel('Epoch')
+plt.ylabel('Loss', rotation=0, labelpad=15)
+plt.legend(loc='upper right')
+plt.show()
+
+plt.plot(testAcc, 'm', linewidth=2.0, alpha=1.0)
+plt.xlim(0, n_epochs)
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy', rotation=0, labelpad=30)
+plt.title('Testing Accuracy')
+plt.show()
--- a1/model.py
+++ a1/model.py
@@ -50,9 +50,9 @@ class linearClassifier:
         D = len(X)
         P = self.evaluate(X)
         
-        loss = [-np.dot(Y[i, :], np.log(P[:, i])) for i in range(D)]
-        J    = 1 / D * sum(loss) + lambd * np.sum(self.W**2)
-        return J
+        l = - 1 / D * sum([np.dot(Y[i, :], np.log(P[:, i])) for i in range(D)])
+        r = lambd * np.sum(self.W**2)
+        return  l + r
     
     def computeAcc(self, X, k):
         """
@@ -70,8 +70,88 @@ class linearClassifier:
         return np.mean([preds == k])
     
     def computeGrads(self, X, Y, lambd):
-        J = self.computeCost(X, Y, lambd)
+        """
+        Parameters
+        ----------
+        X : Nxd data matrix
+        Y : NxK one-hot encoded label matrix
+        lambd: regularization parameter
         
+        Returns
+        -------
+        W_grads : gradients for weight martix (W)
+        b_grads : gradients for bias matrix (b)
+        """
+        D = len(X)
         
-        return 0
+        P = self.evaluate(X)
+        g = -(Y.T - P)
+        
+        W_grads = 1 / D * np.matmul(g, X) + 2 * lambd * self.W
+        b_grads = 1 / D * np.sum(g, axis=1)
+        
+        return W_grads, np.expand_dims(b_grads, axis=1)
 
+    def computeGradsNumerical(self, X, Y, lambd):
+        """
+        Parameters
+        ----------
+        X : Nxd data matrix
+        Y : NxK one-hot encoded label matrix
+        lambd: regularization parameter
+        
+        Returns
+        -------
+        W_gradsNum : numerically calculated gradients for weight martix (W)
+        b_gradsNum : numerically calculated gradients for bias matrix (b)
+        """
+        W_0, b_0 = self.W, self.b
+        eps = 1e-5
+        
+        # calculate numerical gradients for W
+        W_perturb = np.zeros(W_0.shape)
+        W_gradsNum = np.zeros(W_0.shape)
+        for i in range(self.K):
+            for j in range(self.d):
+                W_perturb[i, j] = eps
+                
+                W_tmp = W_0 - W_perturb
+                self.W = W_tmp
+                cost = self.computeCost(X, Y, lambd)
+                
+                W_tmp = W_0 + W_perturb
+                self.W = W_tmp
+                lossDiff = (self.computeCost(X, Y, lambd) - cost) / (2 * eps)
+                
+                W_gradsNum[i, j] = lossDiff
+                W_perturb[i, j] = 0
+            
+        self.W = W_0
+        
+        # calculate numerical gradients for b
+        b_perturb = np.zeros(b_0.shape)
+        b_gradsNum = np.zeros(b_0.shape)
+        for i in range(self.K):
+            b_perturb[i] = eps
+            
+            b_tmp = b_0 - b_perturb
+            self.b = b_tmp
+            cost = self.computeCost(X, Y, lambd)
+            
+            b_tmp = b_0 + b_perturb
+            self.b = b_tmp
+            lossDiff = (self.computeCost(X, Y, lambd) - cost) / (2 * eps)
+            
+            b_gradsNum[i] = lossDiff
+            b_perturb[i] = 0
+            
+        self.b = b_0
+        
+        return W_gradsNum, np.squeeze(b_gradsNum)
+    
+    def train(self, X, Y, lambd, eta):
+        W_grads, b_grads = self.computeGrads(X, Y, lambd)
+        
+        self.W = self.W - eta * W_grads
+        self.b = self.b - eta * b_grads
+        

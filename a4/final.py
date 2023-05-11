8bf63c5db80208b4058f7c7117bf77ef68706b64
oStglnd <stigland@kth.se>
2023-05-12 00:04:10 +0200

    Finished A4

--- a4/final.py
+++ a4/final.py
@@ -1,7 +1,36 @@
 
-
+import os
 import numpy as np
-from misc import softMax
+import matplotlib.pyplot as plt
+
+def softMax(S: np.array) -> np.array:
+    """
+    Parameters
+    ----------
+    S : dxN score matrix
+
+    Returns
+    -------
+    S : dxN score matrix w. applied softmax activation
+    """
+    S = np.exp(S)
+    return S / np.sum(S, axis=0)
+
+def oneHotEncode(k: np.array) -> np.array:
+    """
+    Parameters
+    ----------
+    k : Nx1 label vector
+
+    Returns
+    -------
+    Y: NxK one-hot encoded label matrix
+    """
+    numCats = np.max(k)
+    return np.array([[
+        1 if idx == label else 0 for idx in range(numCats+1)]
+         for label in k]
+    )
 
 class recurrentNeuralNetwork:
     def __init__(
@@ -43,7 +72,16 @@ class recurrentNeuralNetwork:
             x0: np.array,
             n: int
         ) -> list:
-        
+        """
+        Parameters
+        ----------
+        X0 : 1xK initial one-hot encodede entry
+        n : length of sequence
+
+        Returns
+        -------
+        xList : List of generated character indices
+        """
         h = self.hprev
         xList = [x0]
         for _ in range(n):
@@ -73,8 +111,7 @@ class recurrentNeuralNetwork:
         """
         Parameters
         ----------
-        X : Nxd data matrix
-
+        X : seq_len x K one-hot encoded sequence matrix
         Returns
         -------
         P : KxN score matrix w. softmax activation
@@ -119,8 +156,7 @@ class recurrentNeuralNetwork:
         """
         Parameters
         ----------
-        X : Nxd data matrix
-
+        X : seq_len x K one-hot encoded sequence matrix
         Returns
         -------
         P : KxN score matrix w. softmax activation
@@ -136,20 +172,18 @@ class recurrentNeuralNetwork:
     def computeCost(
             self, 
             X: np.array, 
-            Y: np.array, 
-            lambd: float
+            Y: np.array
         ) -> float:
         """
         Parameters
         ----------
-        X : Nxd data matrix
-        Y : NxK one-hot encoded label matrix
+        X : seq_len x K one-hot encoded sequence matrix, at t
+        Y : seq_len x K one-hot encoded sequence matrix, at t+1
         lambd : regularization parameter
         
         Returns
         -------
         l : cross entropy loss
-        J : cost, i.e. cross-entropy loss w. L2-regularization
         """
         # get probs
         P = self.evaluate(X, train=False)
@@ -157,44 +191,22 @@ class recurrentNeuralNetwork:
         # evaluate loss term
         l = - np.sum(Y.T * np.log(P))
         
-        # get regularization term
-        r = 0
-        
-        return l, l + r
-    
-    def computeAcc(
-            self, 
-            X: np.array, 
-            k: np.array
-        ) -> float:
-        """
-        Parameters
-        ----------
-        X : Nxd data matrix
-        k : Nx1 ground-truth label vector
-        Returns
-        -------
-        acc : accuracy score
-        """
-        return 0
+        return l
     
     def computeGrads(
             self, 
             X: np.array, 
-            Y: np.array, 
-            lambd: float
+            Y: np.array
         ) -> (np.array, np.array):
         """
         Parameters
         ----------
-        X : Nxd data matrix
-        Y : NxK one-hot encoded label matrix
-        lambd: regularization parameter
+        X : seq_len x K one-hot encoded sequence matrix, at t
+        Y : seq_len x K one-hot encoded sequence matrix, at t+1
         
         Returns
         -------
-        W_grads : gradients for weight martix (W)
-        b_grads : gradients for bias matrix (b)
+        grads : dictionary w. computed gradients
         """
         P, A, H, O = self.evaluate(X=X, train=True)
         g = -(Y.T - P)
@@ -241,16 +253,14 @@ class recurrentNeuralNetwork:
     def computeGradsNumerical(
             self, 
             X: np.array, 
-            Y: np.array, 
-            lambd: float,
+            Y: np.array,
             eps: float,
         ) -> np.array:
         """
         Parameters
         ----------
-        X : Nxd data matrix
-        Y : NxK one-hot encoded label matrix
-        lambd: regularization parameter
+        X : seq_len x K one-hot encoded sequence matrix, at t
+        Y : seq_len x K one-hot encoded sequence matrix, at t+1
         eps: epsilon for incremental derivative calc.
         
         Returns
@@ -280,13 +290,13 @@ class recurrentNeuralNetwork:
                     # and compute cost
                     w_tmp = w_0 - w_perturb
                     self.weights[name] = w_tmp
-                    _, cost1 = self.computeCost(X, Y, lambd)
+                    _, cost1 = self.computeCost(X, Y)
                 
                     # perturb weight vector positively
                     # and compute cost
                     w_tmp = w_0 + w_perturb
                     self.weights[name] = w_tmp
-                    _, cost2 = self.computeCost(X, Y, lambd)
+                    _, cost2 = self.computeCost(X, Y)
                     lossDiff = (cost2 - cost1) / (2 * eps)
                     
                     # get numerical grad f. W[i, j]
@@ -304,21 +314,19 @@ class recurrentNeuralNetwork:
     def train(
             self, 
             X: np.array, 
-            Y: np.array, 
-            lambd: float, 
+            Y: np.array,
             eta: float
         ):
         """
         Parameters
         ----------
-        X : Nxd data matrix
-        Y : NxK one-hot encoded label matrix
-        lambd: regularization parameter
+        X : seq_len x K one-hot encoded sequence matrix, at t
+        Y : seq_len x K one-hot encoded sequence matrix, at t+1
         eta: learning rate
         """
         # get grads from self.computeGrads and update weights
         # w. GD and learning parameter eta
-        grads = self.computeGrads(X, Y, lambd)
+        grads = self.computeGrads(X, Y)
         for key, weight in self.weights.items():
             # clip gradient
             grads[key] = np.clip(grads[key], -5, 5)
@@ -328,3 +336,122 @@ class recurrentNeuralNetwork:
             
             # update weight
             weight -= eta * grads[key] / np.sqrt(self.momentum[key] + 1e-12)
+
+def main():
+    # get paths
+    home_path = os.path.dirname(os.getcwd())
+    data_path = home_path + '\\data\\a4\\'
+    plot_path = home_path + '\\a4\\plots\\'
+    # results_path = home_path + '\\a4\\results\\'
+    
+    # get text data
+    fname = 'goblet_book.txt'
+    fpath = data_path + fname
+    
+    # read text file
+    with open(fpath, 'r') as fo:
+        data = fo.readlines()
+        
+    # split lines into words and words into chars
+    data = [char 
+                for line in data
+                    for word in list(line)
+                        for char in list(word)]
+    
+    # create word-key-word mapping
+    keyToChar = dict(enumerate(np.unique(data)))
+    charToKey = dict([(val, key) for key, val in keyToChar.items()])
+    
+    # define params
+    K  = len(keyToChar)
+    m = 100
+    sigma = 0.01
+    seq_length = 25
+    
+    # define X, w. one-hot encoded representations
+    data = oneHotEncode(np.array([charToKey[char] for char in data]))
+    X = []
+    for i in range(len(data) - seq_length):
+        X.append(data[i:i+seq_length])
+    
+    # init networks
+    recurrentNet = recurrentNeuralNetwork(
+        K=K,
+        m=m,
+        sigma=sigma,
+        seed=2
+    )
+    
+    # save best weights
+    weights_best = recurrentNet.weights.copy()
+    
+    epoch_n = 0
+    print ('\n------EPOCH {}--------\n'.format(epoch_n))
+    
+    lossHist = []
+    loss_smooth = recurrentNet.computeCost(X[0], X[1])
+    loss_best = loss_smooth
+    
+    n = len(X)
+    e = 0
+    for i in range(2000000):
+        recurrentNet.train(X[e], X[e+1], eta=0.1)
+        loss = recurrentNet.computeCost(X[e], X[e+1])
+        
+        loss_smooth = 0.999 * loss_smooth + 0.001 * loss
+        if loss_smooth < loss_best:
+            weights_best = recurrentNet.weights.copy()
+            loss_best = loss_smooth
+    
+        if (i % 10 == 0) and i > 0:
+            lossHist.append(loss_smooth)
+            
+            if i % 1000 == 0:
+                print('Iteration {}, LOSS: {}'.format(i, loss_smooth))
+            
+        if i % 10000 == 0:
+            sequence = recurrentNet.synthesizeText(
+                x0=X[e+1][:1], 
+                n=250
+            )
+            
+            # convert to chars and print sequence
+            sequence = ''.join([keyToChar[key] for key in sequence])
+            print('\nGenerated sequence \n\t {}\n'.format(sequence))
+            
+        # update e
+        if e < (n - seq_length):
+            e += seq_length
+        else:
+            e = 0
+            recurrentNet.hprev = np.zeros(shape=(m, 1))
+            
+            epoch_n += 1
+            print ('\n------EPOCH {}--------\n'.format(epoch_n))
+            
+            if epoch_n >= 4:
+                break
+                
+    # plot results
+    steps = [step * 10 for step in range(len(lossHist))]
+    plt.plot(steps, lossHist, 'r', linewidth=1.5, alpha=1.0, label='Loss')
+    plt.xlim(0, steps[-1])
+    plt.xlabel('Step')
+    plt.ylabel('', rotation=0, labelpad=20)
+    plt.title('Smooth loss for $4$ epochs')
+    # plt.legend(loc='upper right')
+    # plt.savefig(plot_path + 'rnn_loss.png', dpi=200)
+    plt.show()
+    
+    recurrentNet.weights = weights_best
+    sequence = recurrentNet.synthesizeText(
+        x0=X[e+1][:1], 
+        n=1000
+    )
+    
+    # convert to chars and print sequence
+    sequence = ''.join([keyToChar[key] for key in sequence])
+    print('\nGenerated sequence \n\t {}\n'.format(sequence))
+    
+if __name__ == '__main__':
+    main()
